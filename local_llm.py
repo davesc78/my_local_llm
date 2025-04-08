b5322f93573d00f5edb71303b6816455686e7daa
davesc78 <davesc78@gmail.com>
2025-04-08 09:39:48 -0400

    modified:   local_llm.py

--- local_llm.py
+++ local_llm.py
@@ -8,9 +8,7 @@ from transformers import BitsAndBytesConfig
 
 model_id = "meta-llama/Llama-3.2-3B-Instruct"
 # model_id = "meta-llama/Llama-3.2-1B-Instruct"
-### Probar con microsoft/Phi-3-medium-4k-instruct  
-
-
+### Probar con microsoft/Phi-3-medium-4k-instruct
 
 
 tokenizer = AutoTokenizer.from_pretrained(model_id)
@@ -40,10 +38,11 @@ pipe = pipeline(
 )
 
 
-
-
 messages = [
-    {"role": "system", "content": "You are a pirate chatbot who always responds in pirate speak!"},
+    {
+        "role": "system",
+        "content": "You are a pirate chatbot who always responds in pirate speak!",
+    },
     {"role": "user", "content": "Who are you?"},
 ]
 
@@ -57,22 +56,24 @@ print(outputs[0]["generated_text"][-1])
 outputs
 
 
-
 def ask(user_q):
-  messages = [
-    {"role": "system", "content": "You are a helpful assistant. Your job is to answer the question of the user"},
-    {"role": "user", "content": user_q},
-  ]
-  outputs = pipe(
-    messages,
-    max_new_tokens=1000,
-  )
-  assistant_response =  outputs[0]["generated_text"][-1]['content']
-  print(assistant_response)
-  
-  
-  
+    messages = [
+        {
+            "role": "system",
+            "content": "You are a helpful assistant. Your job is to answer the question of the user",
+        },
+        {"role": "user", "content": user_q},
+    ]
+    outputs = pipe(
+        messages,
+        max_new_tokens=1000,
+    )
+    assistant_response = outputs[0]["generated_text"][-1]["content"]
+    print(assistant_response)
+
 
 ask("Hola, soy de Chile, y quiero saber como es el clima en mi país.")
 ask("¿Sabes qué es ChileCompra")
-ask('Si yo vendo pan amasado, ¿Me conviene ofertar a una licitación de pan para la municiaplidad de Santiago?')
+ask(
+    "Si yo vendo pan amasado, ¿Me conviene ofertar a una licitación de pan para la municiaplidad de Santiago?"
+)
